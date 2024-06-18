f4aa736d199b7deaab26647cb819dfa8e2a62de0
feraa123 <fera_darmayanti@teknokrat.ac.id>
2024-06-18 20:20:25 +0700

    fera: enableedgetoedge

--- Program/app/src/main/java/com/uti/ta_pm_22a_teamlontong/RegisterActivity.kt
+++ Program/app/src/main/java/com/uti/ta_pm_22a_teamlontong/RegisterActivity.kt
@@ -37,4 +37,12 @@ class RegisterActivity : AppCompatActivity() {
             finish()
         }
 
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
 }
