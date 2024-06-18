86c0dafbc910e06acdde550937101c63aadc32b2
contechs70 <contechs70@gmail.com>
2024-06-18 19:55:16 +0700

    master: memnambahkan enabeltoedge dalam event txRegis

--- Program/app/src/main/java/com/uti/ta_pm_22a_teamlontong/LoginActivity.kt
+++ Program/app/src/main/java/com/uti/ta_pm_22a_teamlontong/LoginActivity.kt
@@ -40,4 +40,12 @@ class LoginActivity : AppCompatActivity() {
             finish()
         }
 
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
 }
